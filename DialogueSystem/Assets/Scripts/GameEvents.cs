08b6de907bd78d67e3aa8412e9aacb58a1e9dcd2
Melissa Gray <61853436+mag535@users.noreply.github.com>
2023-03-10 23:21:29 -0500

    started audio manager
    still need to get audio to actually play

--- DialogueSystem/Assets/Scripts/GameEvents.cs
+++ DialogueSystem/Assets/Scripts/GameEvents.cs
@@ -13,12 +13,17 @@ public class ShowDialogueText : EvtSystem.Event
 public class PlayAudio : EvtSystem.Event
 {
     public AudioClip clipToPlay;
+
+    public float volume;
+    public float pitch;
 }
 
 public struct ResponseData
 {
     public string text;
     public int karmaScore;
+
+    public AudioClip dialogueAudio;
 
     public UnityAction buttonAction;
 }
