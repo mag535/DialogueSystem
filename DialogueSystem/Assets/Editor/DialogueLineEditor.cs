840fbb0ff1484b963fe7fac9ad38c53c625b2040
Melissa Gray <61853436+mag535@users.noreply.github.com>
2023-03-18 16:08:41 -0400

    finished dialogue line data editor

--- DialogueSystem/Assets/Editor/DialogueLineEditor.cs
+++ DialogueSystem/Assets/Editor/DialogueLineEditor.cs
@@ -12,27 +12,30 @@ public class DialogueLineEditor : Editor
     {
         // Each editor window contains a root VisualElement object
         var editorAsset = AssetDatabase.
-            LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/DialogueLineEditor.uxml"); ;
+            LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/DialogueLineEditor.uxml");
 
         var root = editorAsset.CloneTree();
 
-        // Add name of dialogue line data to editor
         targetLine = target as DialogueLineData;
+
+        root.style.flexGrow = 1.0f;
 
         var nameLabel = root.Query<Label>("NameLabel").First();
         nameLabel.text = targetLine.name;
 
-        // add audio clip to editor
+        var groupBox = root.Query<GroupBox>("Options").First();
+
         var audioClip = new ObjectField();
         audioClip.objectType = typeof(AudioClip);
         audioClip.bindingPath = "dialogueAudio";
         audioClip.Bind(serializedObject);
+        audioClip.label = "Dialogue Clip";
 
-        root.Add(audioClip);
-        
-        // VisualElements objects can contain other VisualElement following a tree hierarchy.
-        VisualElement label = new Label("Hello World! From C#");
-        root.Add(label);
+        groupBox.Add(audioClip);
+
+        SerializedProperty responseArray = serializedObject.FindProperty("responses");
+        var responseField = new PropertyField(responseArray);
+        root.Add(responseField);
 
         return root;
     }
